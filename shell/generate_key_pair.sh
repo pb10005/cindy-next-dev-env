4b0e6bd69436f9c9cdfbae56000c695083dd4f5b
pb10001 <atihsa.mihs.ne.ho.da@gmail.com>
2020-05-05 17:11:52 +0900

    Update generate_key_pair.sh

--- shell/generate_key_pair.sh
+++ shell/generate_key_pair.sh
@@ -1,3 +1,3 @@
 ssh-keygen -f id_rsa
-ssh-keygen -f id_rsa -e -m pem > /usr/app/cindy/cindy-next/private.pem
-mv id_rsa.pub /usr/app/cindy/cindy-next/public.pem
+ssh-keygen -f id_rsa -e -m pem > /usr/app/cindy/private.pem
+mv id_rsa.pub /usr/app/cindy/public.pem
